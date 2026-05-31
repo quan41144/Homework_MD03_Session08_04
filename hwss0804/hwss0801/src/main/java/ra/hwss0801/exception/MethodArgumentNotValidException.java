d2eed6d108b70d8218e0d2a09554920b97667579
quan41144 <quan41144@gmail.com>
2026-05-31 20:22:15 +0700

    Delete hwss0804 directory

--- hwss0804/hwss0801/src/main/java/ra/hwss0801/exception/MethodArgumentNotValidException.java
+++
@@ -1,9 +0,0 @@
-package ra.hwss0801.exception;
-
-import org.springframework.validation.FieldError;
-
-public class MethodArgumentNotValidException extends RuntimeException {
-    public MethodArgumentNotValidException(String message) {
-        super(message);
-    }
-}
